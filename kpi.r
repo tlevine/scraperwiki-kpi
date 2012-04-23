f62d727c44b4c0f4459b5d21096505ad324294ed
Thomas Levine <thomas.levine@gmail.com>
2012-04-23 12:58:03 -0400

    aoeuaoue

--- kpi.r
+++ kpi.r
@@ -1,4 +1,10 @@
 #!/usr/bin/env Rscript
+
+library(reshape2)
+library(plyr)
+library(ggplot2)
+library(Cairo)
+
 raw <- read.csv(
   # This is a dump of the username, date_joined and last_login columns
   # from the auth_users Django table.
@@ -8,11 +14,42 @@ raw <- read.csv(
 )
 #print(raw[sample(nrow(raw), 3),])
 
-raw$last_login <- as.POSIXlt(raw$last_login)
-raw$date_joined <- as.POSIXlt(raw$date_joined)
+raw$last_login <- as.POSIXct(raw$last_login)
+raw$date_joined <- as.POSIXct(raw$date_joined)
 
-kpi <- melt(raw, 'username', variable.name = 'event', value.name = 'datetime')
-colnames(kpi)[2:3] <- c('event', 'datetime') # Dunno why it doesn't work above
+# Subset for testing
+raw <- raw[sample.int(nrow(raw), 100),]
 
-#rownames(raw) <- raw$username
-#raw$username <- NULL
+raw$active_time <- as.numeric(difftime(raw$last_login, raw$date_joined, units = 'days'))
+kpi <- melt(raw,
+  id.vars = colnames(raw), measure.vars = c('date_joined', 'last_login'),
+  variable.name = 'event', value.name = 'datetime'
+)
+colnames(kpi)[-(1:(ncol(kpi)-2))] <- c('event', 'datetime') # Dunno why it doesn't work above
+
+p <- ggplot(kpi) +
+  aes(x = datetime, group = username) +
+  scale_x_datetime('Span of activity, from user registration to most recent login') +
+  opts(
+    title = 'ScraperWiki Coder Activity',
+    theme_text(family = "sans", face = "bold")
+  ) +
+# aes(label = username) + geom_text() + #Usernames
+  geom_line(color = alpha('black', 0.1))
+
+plots <- list(
+  duration = p + aes(y = active_time) + scale_y_continuous('Users sorted by length of activity'),
+  last_login = p + aes(y = last_login) + scale_y_datetime('Users sorted by date of most recent login'),
+  date_joined = p + aes(y = date_joined) + scale_y_datetime('Users sorted by date joined login')
+)
+
+# Plot them
+print('Plotting...')
+
+#pdf('coder_activity.pdf', paper = 'a4r', fonts = 'Helvetica')
+# CairoPDF kerns fonts properly
+CairoPDF('coder_activity.pdf', paper = 'a4r', pointsize = 10)
+l_ply(plots, print)
+dev.off()
+
+print('Finished plotting')
