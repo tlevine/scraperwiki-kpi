9e02a6cb4165699d5a4aafb5c2267b19cef0833f
Thomas Levine <thomas.levine@gmail.com>
2012-04-24 11:06:44 -0400

    (longtime) (active) coder types

--- kpi.r
+++ kpi.r
@@ -20,6 +20,19 @@ kpi.raw$last_login <- as.POSIXct(kpi.raw$last_login)
 kpi.raw$date_joined <- as.POSIXct(kpi.raw$date_joined)
 kpi.raw$active_time <- as.numeric(difftime(kpi.raw$last_login, kpi.raw$date_joined, units = 'days'))
 
+coder_type <- function(raw){
+  # Is this a longtime active coder?
+  one_month_ago <- as.POSIXct('2012-03-23')
+  raw$active <- raw$last_login > one_month_ago
+  raw$longtime <- raw$date_joined < one_month_ago
+  raw$coder_type <- 'Inactive Coder'
+  raw$coder_type[raw$active] <-
+    'Pseudo Active Coder' # (user who logged in at least once in the last month)'
+  raw$coder_type[raw$active & raw$longtime] <-
+    'Pseudo Longtime Active Coder' # (active coder who joined in an earlier month)'
+  raw$coder_type
+}
+
 plot.kpi <- function(
   raw,
   sort.yvar = NULL,
@@ -53,7 +66,7 @@ plot.kpi <- function(
   # kpi <- subset(kpi, script_count > 2)
 
   p <- ggplot(kpi) +
-    aes(x = datetime, group = username) +
+    aes(x = datetime, group = username, color = longtime_active) +
     scale_x_datetime('Span of activity, from user registration to most recent login',
       format = DATEFORMAT, major = "3 months", minor = "1 month"
     ) +
@@ -62,7 +75,7 @@ plot.kpi <- function(
       theme_text(family = "sans", face = "bold"),
       panel.background = theme_rect(fill = NA, colour = NA) # Clear background
     ) +
-    geom_line(color = alpha('black', 0.2))
+    geom_line(alpha = 0.2)
 
   p
 }
